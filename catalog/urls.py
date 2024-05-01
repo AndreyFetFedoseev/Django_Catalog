f46f06cbd98fb9f41c48186b6aadb481dc8ea126
Andrey Fedoseev <MaxviP100@yandex.ru>
2024-05-01 15:36:42 +0700

    Task №3, 4 Create controllers views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 
-from catalog.views import homepage
+from catalog.views import homepage, contacts
+
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', homepage)
+    path('', homepage),
+    path('contacts/', contacts)
 ]
