7e1f251d5db39c4a37d4eb17aefad967c38ce53d
Andrey Fedoseev <MaxviP100@yandex.ru>
2024-05-14 22:07:37 +0700

    task №4 migrate

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Category(models.Model):
+    """
+    Модель для категории товаров
+    """
+
+    name = models.CharField(
+        max_length=50, verbose_name="Категория", help_text="Введите категорию товара"
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание категории", **NULLABLE
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+
+class Product(models.Model):
+    """
+    Модель для товаров
+    """
+
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Наименование",
+        help_text="Введите наименование товара",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание товара", **NULLABLE
+    )
+    image = models.ImageField(
+        upload_to="catalog/media",
+        verbose_name="Изображение товара",
+        help_text="Загрузите изображение товара",
+        **NULLABLE
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Введите название категории",
+        **NULLABLE,
+        related_name="products"
+    )
+    price = models.IntegerField(
+        verbose_name="Цена за покупку", help_text="Введите цену"
+    )
+    created_at = models.DateField(
+        verbose_name="Дата создания", help_text="Введите дату", auto_now_add=True
+    )
+    update_at = models.DateField(
+        verbose_name="Дата изменения", help_text="Введите дату", auto_now=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["category", "price"]
