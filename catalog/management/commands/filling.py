5f8531490578203e6790a97f8e14099d01da5d63
Andrey Fedoseev <MaxviP100@yandex.ru>
2024-05-15 23:02:45 +0700

    task №6 command

--- catalog/management/commands/filling.py
+++ catalog/management/commands/filling.py
@@ -12,23 +12,21 @@ class Command(BaseCommand):
         """
         Получаем данные из фикстур с категориями
         """
-    # Здесь мы получаем данные из фикстур с категориями
-    with open('catalog/fixture/catalog_data.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
+        # Здесь мы получаем данные из фикстур с категориями
+        with open("C:/Users/пку/PycharmProjects/Skypro/Django_Catalog/catalog/fixture/catalog_data.json", 'r',
+                  encoding='utf-8') as file:
+            return json.load(file)
 
     def handle(self, *args, **options):
         # Удалите все продукты
         # Удалите все категории
         Category.objects.all().delete()
-
         # Создайте список для хранения объектов
         category_for_create = []
-
         # Обходим все значения категорий из фиктсуры для получения информации об одном объекте
         for category in Command.json_read_categories():
             category_for_create.append(
-                Category(название_поля=значение_из_словаря, ..., название_поля=значение_из_словаря)
+                Category(name=category['fields'].get('name'), description=category['fields'].get('description'))
             )
-
         # Создаем объекты в базе с помощью метода bulk_create()
         Category.objects.bulk_create(category_for_create)
