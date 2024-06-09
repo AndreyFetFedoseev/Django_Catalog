7de6fd16492038e02fe3a7f997c2201796aa2bf3
Andrey Fedoseev <MaxviP100@yandex.ru>
2024-06-09 19:20:59 +0700

    create StyleMixin create word_danger in forms

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,30 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
 
 from catalog.models import Product
 
 
-class ProductForm(ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
         exclude = ('created_at', 'updated_at',)
+
+    def clean_name(self):
+        word_danger = ('казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар')
+        cleaned_data = self.cleaned_data['name']
+        if cleaned_data in word_danger:
+            raise forms.ValidationError('Название запрещено. Введите другое название!')
+        return cleaned_data
+
+    def clean_description(self):
+        word_danger = ('казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар')
+        cleaned_data = self.cleaned_data['description'].lower().replace(',', "").split()
+        if list(set(cleaned_data) & set(word_danger)):
+            raise forms.ValidationError('Запрещенные слова!')
+        return cleaned_data
