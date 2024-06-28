0bad20fd4d1ea9faef2b7f71511775ea6556ae4f
Andrey Fedoseev <MaxviP100@yandex.ru>
2024-06-28 22:47:43 +0700

    create_moderator

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,14 @@
 import json
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.forms import inlineformset_factory
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 from pytils.translit import slugify
 
-from catalog.forms import ProductForm, VersionForm
+from catalog.forms import ProductForm, VersionForm, ProductModeratorForm
 # from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product, Category, Blog, Version
@@ -102,6 +103,15 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
         else:
             return self.render_to_response(self.get_context_data(form=form, formset=formset))
 
+    def get_form_class(self):
+        user = self.request.user
+        if user == self.object.owner:
+            return ProductForm
+        elif user.has_perm('can_change_description_product') and user.has_perm(
+                'can_change_category_product') and user.has_perm('can_change_publication_product'):
+            return ProductModeratorForm
+        raise PermissionDenied
+
 
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
@@ -159,7 +169,6 @@ class BlogDetailView(DetailView):
         self.object.number_of_views += 1
         self.object.save()
         return self.object
-
 
 # class VersionCreateView(CreateView):
 #     model = Version
